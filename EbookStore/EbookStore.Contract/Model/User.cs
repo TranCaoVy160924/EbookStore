b7eca843b5c8fefc8e2ca027f87dd68eb789e243
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-01-15 19:49:14 +0700

    Update User.cs

--- EbookStore/EbookStore.Contract/Model/User.cs
+++ EbookStore/EbookStore.Contract/Model/User.cs
@@ -9,11 +9,9 @@ namespace EbookStore.Contract.Model
 {
     public class User: IdentityUser<Guid>
     {
-        //public int UserID { get; set; }
-        //public string Username { get; set; }
+
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public string Password { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Status { get; set; }
