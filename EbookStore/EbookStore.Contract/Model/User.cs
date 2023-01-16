31952104c0145bf6435afb63822ad040e68060b1
dknhienFPT <105194032+dknhienFPT@users.noreply.github.com>
2023-01-16 17:36:46 +0700

    data seed

--- EbookStore/EbookStore.Contract/Model/User.cs
+++ EbookStore/EbookStore.Contract/Model/User.cs
@@ -14,7 +14,7 @@ namespace EbookStore.Contract.Model
         public string LastName { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public string Status { get; set; }
+        public bool IsActive { get; set; }
 
         public List<WishItem>? WishList { get; set; }
 
