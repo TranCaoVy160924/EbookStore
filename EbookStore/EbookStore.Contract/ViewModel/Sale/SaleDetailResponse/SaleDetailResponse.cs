9226a361f05824d8ccebc53a1ee89f979ec0cb38
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-02-17 07:42:27 +0700

    Sale Detail

--- EbookStore/EbookStore.Contract/ViewModel/Sale/SaleDetailResponse/SaleDetailResponse.cs
+++ EbookStore/EbookStore.Contract/ViewModel/Sale/SaleDetailResponse/SaleDetailResponse.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EbookStore.Contract.ViewModel.Sale.SaleResponse;
-public class SaleResponse
+namespace EbookStore.Contract.ViewModel.Sale.SaleDetailResponse;
+public class SaleDetailResponse
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public double SalePercent { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
+
+    public virtual List<int> OnSaleBookIds { get; set; }
 }
