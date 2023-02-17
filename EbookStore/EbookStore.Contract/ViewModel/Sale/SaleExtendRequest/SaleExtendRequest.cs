ab52fb4c38df8b37dec9d0fef698056e6a73f05a
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-02-17 07:38:46 +0700

    Initial

---
+++ EbookStore/EbookStore.Contract/ViewModel/Sale/SaleExtendRequest/SaleExtendRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbookStore.Contract.ViewModel.Sale.SaleExtendRequest;
+public class SaleExtendRequest
+{
+    [Required(ErrorMessage = "Please enter extended sale id")]
+    public int SaleId { get; set; }
+
+    [DataType(DataType.Date)]
+    [Required(ErrorMessage = "Please enter new sale end date")]
+    public DateTime NewEndDate { get; set; }
+}
---
+++ EbookStore/EbookStore.Contract/ViewModel/Sale/SaleResponse/SaleResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbookStore.Contract.ViewModel.Sale.SaleResponse;
+public class SaleResponse
+{
+    public int SaleId { get; set; }
+    public string Name { get; set; }
+    public double SalePercent { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+}
