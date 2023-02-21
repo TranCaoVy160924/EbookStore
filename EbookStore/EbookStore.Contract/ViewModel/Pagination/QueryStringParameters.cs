98e85c6a5ef0c17caeb59bba427dfa6fb9c37161
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-02-21 10:12:57 +0700

    Data annotation

--- EbookStore/EbookStore.Contract/ViewModel/Pagination/QueryStringParameters.cs
+++ EbookStore/EbookStore.Contract/ViewModel/Pagination/QueryStringParameters.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,14 @@ namespace EbookStore.Contract.ViewModel.Pagination;
 public abstract class QueryStringParameters
 {
     const int maxPageSize = 50;
+    
+    [Range(1, double.MaxValue, ErrorMessage = "Page number must greater than 0")]
     public int PageNumber { get; set; } = 1;
+
+
     private int _pageSize = 10;
+
+    [Range(1, 50, ErrorMessage = "Page size must be between 1 and 50")]
     public int PageSize
     {
         get
