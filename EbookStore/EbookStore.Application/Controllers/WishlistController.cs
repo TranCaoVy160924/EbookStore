faf2a0d1d9ff04479de78514c6cfb53d5ebb3975
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-02-25 09:58:10 +0700

    fix merge conflict

--- EbookStore/EbookStore.Application/Controllers/WishlistController.cs
+++ EbookStore/EbookStore.Application/Controllers/WishlistController.cs
@@ -1,6 +1,9 @@
 ﻿using EbookStore.Application.Helpers;
+using EbookStore.Contract.Model;
+using EbookStore.Contract.ViewModel.WishItem.Request;
 using EbookStore.Domain.Repository.WishlistRepo;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,10 +15,43 @@ namespace EbookStore.Application.Controllers;
 public class WishlistController : ControllerBase
 {
     private readonly IWishlistRepository _wishlistRepo;
+    private readonly UserManager<User> _userManager;
 
-    public WishlistController(IWishlistRepository wishlistRepo)
+    public WishlistController(
+        IWishlistRepository wishlistRepo,
+        UserManager<User> userManager)
     {
         _wishlistRepo = wishlistRepo;
+        _userManager = userManager;
+    }
+
+    [HttpPost("Search")]
+    [Authorize]
+    public async Task<IActionResult> GetAsync([FromBody] WishItemQueryRequest queryRequest)
+    {
+        try
+        {
+            var pagedResult = await _wishlistRepo.GetAsync(queryRequest, GetUserId());
+
+            Response.Headers.Add("X-Pagination", pagedResult.GetMetadata());
+
+            return Ok(pagedResult.Data);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    private async Task<Guid> GetUserId()
+    {
+        var claims = User.Claims.ToList();
+
+        var userName = claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+
+        User user = await _userManager.FindByNameAsync(userName);
+
+        return user.Id;
     }
 
     [HttpPost("{bookId}")]
