6e86a32c9a5a80081e576f265cb1112822a9d948
phuthang4444 <101709482+phuthang4444@users.noreply.github.com>
2023-03-01 14:27:37 +0700

    FE sale list

--- EbookStore/EbookStore.Client/Controllers/SaleController.cs
+++ EbookStore/EbookStore.Client/Controllers/SaleController.cs
@@ -1,29 +1,32 @@
-﻿using EbookStore.Client.Services;
+﻿namespace EbookStore.Client.Controllers;
+
+using EbookStore.Client.RefitClient;
+using EbookStore.Contract.ViewModel.Pagination;
+using EbookStore.Contract.ViewModel.Sale.Request;
 using EbookStore.Contract.ViewModel.Sale.Response;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Refit;
 
-namespace EbookStore.Client.Controllers
-{
-    public class SaleController : Controller
+public class SaleController: Controller
+{   
+    public readonly ISaleClient _saleClient;
+    private readonly string _jwtToken;
+    private PaginationHeader PaginationMetadata;
+
+    public List<SaleResponse> Data { get; private set; }
+    
+    private async Task LoadSaleData(SaleQueryRequest saleQueryRequest)
     {
-        private readonly ISaleService _saleService;
-
-        public SaleController(ISaleService saleService)
-        {
-            _saleService = saleService;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            var sales = await _saleService.GetSalesAsync();
-            var saleResponses = sales.ConvertAll(s => new SaleResponse
-            {
-                Id = s.Id,
-                SalePercentage = s.SalePercentage,
-                SaleDate = s.SaleDate
-            });
-            return View(saleResponses);
-        }
+        ApiResponse<List<SaleResponse>> response
+        = await _saleClient.GetResponseAsync(saleQueryRequest, _jwtToken);
+        string headerString = await response.GetPaginationHeader();
+        PaginationMetadata = JsonConvert.DeserializeObject<PaginationHeader>(headerString);
+        Data = response.ReadResult();
     }
+    public IActionResult Index(ISaleClient saleClient) {
+
+        return View();
+    }
+
 }
---
+++ EbookStore/EbookStore.Client/RefitClient/ISaleClient.cs
@@ -0,0 +1,21 @@
+﻿using EbookStore.Contract.Model;
+using EbookStore.Contract.ViewModel.Sale.Request;
+using EbookStore.Contract.ViewModel.Sale.Response;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EbookStore.Client.RefitClient;
+
+    [Headers("Content-Type: application/json")]
+    public interface ISaleClient
+    {
+        [Post("/Sale/Search")]
+        Task<ApiResponse<List<SaleResponse>>> GetResponseAsync([Body] SaleQueryRequest queryRequest,
+            [Header("Authorization")] string jwtToken);
+
+    }
