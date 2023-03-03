264b1b60f608c9daa134d661039d0d281b427696
phuthang4444 <101709482+phuthang4444@users.noreply.github.com>
2023-03-03 13:17:48 +0700

    update for extended sale

--- EbookStore/EbookStore.Client/Controllers/SaleController.cs
+++ EbookStore/EbookStore.Client/Controllers/SaleController.cs
@@ -1,23 +1,46 @@
 ﻿namespace EbookStore.Client.Controllers;
 
+using EbookStore.Client.Helper;
 using EbookStore.Client.RefitClient;
 using EbookStore.Contract.ViewModel.Pagination;
 using EbookStore.Contract.ViewModel.Sale.Request;
 using EbookStore.Contract.ViewModel.Sale.Response;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using Refit;
 
-public class SaleController: Controller
+[Authorize]
+public class SaleController : Controller
 {
     private readonly ISaleClient _saleClient;
     public SaleController(ISaleClient saleClient)
     {
         _saleClient = saleClient;
     }
-    public IActionResult Index() {
 
-        return View();
+    [HttpPost]
+    public async Task<IActionResult> Update(SaleExtendRequest saleExtendRequest)
+    {
+        UserManager userManager = new UserManager(this.User);
+        if (ModelState.IsValid)
+        {
+            if (userManager.IsLogin())
+            {
+                var token = userManager.GetToken();
+                saleExtendRequest = new SaleExtendRequest
+                {
+                    SaleId = saleExtendRequest.SaleId,
+                    NewEndDate = saleExtendRequest.NewEndDate
+                };
+                await _saleClient.ExtendSaleAsync(saleExtendRequest, token);
+                return RedirectToAction("Index");
+            }
+        }
+        return View(saleExtendRequest);
     }
 
+    public IActionResult Index()
+    {
+        return View();
+    }
 }
