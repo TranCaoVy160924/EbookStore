9d058921eedb5a195f4fed36782186abd4ef3a75
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-03-16 15:54:35 +0700

    change ebook hosting service to self hosting

--- EbookStore/EbookStore.Client/ExternalService/EbookHostService/DropboxHelper.cs
+++ EbookStore/EbookStore.Client/ExternalService/EbookHostService/DropboxHelper.cs
@@ -1,27 +1,37 @@
 ﻿using Dropbox.Api;
 using Dropbox.Api.Files;
+using EbookStore.Client.RefitClient;
+using Microsoft.AspNetCore.DataProtection;
+using NuGet.Configuration;
+using System.Net.Http;
 using System.Text;
 
 namespace EbookStore.Client.ExternalService.EbookHostService;
 
 public class DropboxHelper : IEbookHostHelper
 {
-    private readonly DropboxClient _dropbox;
-    private const string TOKEN = "sl.BaR0DFXD16WWsyQLNjmECsfw4KxOhK6MfIEQGV7Z4q_3Tb_HK-x7dURoaYZ3sMetEjyRGi-2_EVkOeQP-OwyBWTXbvn7GtIfYcNFODcsX87eX8OKeWpF9rHLTlbVWAVYoMdMA1_6ViTf";
+    private DropboxClient Dropbox;
+    private readonly IDropboxClient _dropboxRefitClient;
+    private const string TOKEN = "Y1h6HFGgbQ4AAAAAAAAAFRH2GmsvtyFU3L5C0Z3uMyg";
+    private const string API_KEY = "rwv3peovwrk07a5";
+    private const string API_SECRET = "en27g1yz91a3nvz";
     private const string FOLDER = "/sdfg";
 
-    public DropboxHelper()
+    public DropboxHelper(IDropboxClient dropboxRefitClient)
     {
-        _dropbox = new DropboxClient(TOKEN);
+        _dropboxRefitClient = dropboxRefitClient;
+        Dropbox = new DropboxClient(API_KEY, API_SECRET);
     }
 
     public async Task<string> Upload(IFormFile ebookFile)
     {
+        //await GetDropbox();
+
         var fileName = ebookFile.FileName;
         using (var mem = new MemoryStream())
         {
             ebookFile.OpenReadStream().CopyTo(mem);
-            var updated = await _dropbox.Files.UploadAsync(
+            var updated = await Dropbox.Files.UploadAsync(
                 FOLDER + "/" + fileName,
                 WriteMode.Overwrite.Instance,
                 body: mem);
@@ -29,4 +39,30 @@ public class DropboxHelper : IEbookHostHelper
 
         return fileName;
     }
+
+    private async Task GetDropbox()
+    {
+        string authToken = await _dropboxRefitClient.GetAuthTokenAsync(API_KEY);
+        Dropbox = new DropboxClient(TOKEN);
+    }
+
+    //private async Task GetToken()
+    //{
+    //    // Instantiate a DropboxClient with your app key and app secret
+    //    var client = new DropboxClient(API_KEY, API_SECRET);
+
+    //    // Generate an authorization URL
+    //    var authorizeUri = client.Auth.GetAuthorizeUri(new string[] { "files.metadata.read" });
+
+    //    // Redirect the user to the authorization URL
+
+    //    // When the user is redirected back to your redirect URI, retrieve the authorization code
+    //    string authorizationCode = "<authorization-code>";
+
+    //    // Exchange the authorization code for an access token
+    //    var token = await client.Auth.TokenFromOAuth1Async(new OAuth1AuthorizationCode(authorizationCode));
+
+    //    // Store the access token for future use
+    //    var accessToken = token.OAuthToken;
+    //}
 }
