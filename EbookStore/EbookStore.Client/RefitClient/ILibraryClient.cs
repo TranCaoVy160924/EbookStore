74da5843db60df9a301cf4937b08ed3938833c91
Pham Minh Duy <duypmse150376@fpt.edu.vn>
2023-03-21 15:20:03 +0000

    Merged in Library (pull request #35)
    Library
    
    * done library
    
    * Merge branch 'main' into Library
    
    * fix library
    
    * commit
    
    * fix loi
    
    * Merge branch 'main' into Library
    
    * download ebook
    
    
    Approved-by: Tran Cao Vy (K16_HCM)

---
+++ EbookStore/EbookStore.Client/RefitClient/ILibraryClient.cs
@@ -0,0 +1,11 @@
+﻿using EbookStore.Contract.ViewModel.LibraryItem.Response;
+using EbookStore.Contract.ViewModel.LibraryItem.Request;
+using Refit;
+
+namespace EbookStore.Client.RefitClient;
+
+public interface ILibraryClient
+{
+    [Post("/LibraryItem/Search")]
+    Task<ApiResponse<List<LibraryItemResponse>>> GetResponseAsync([Body] LibraryItemQueryRequest queryRequest, [Header("Authorization")] string jwtToken);
+}
