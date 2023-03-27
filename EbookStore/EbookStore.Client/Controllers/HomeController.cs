4313a030abc796a2906d5b1a14b289922fa5e396
TranCaoVy160924 <vytcse160924@fpt.edu.vn>
2023-03-27 13:29:50 +0700

    fix

--- EbookStore/EbookStore.Client/Controllers/HomeController.cs
+++ EbookStore/EbookStore.Client/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using EbookStore.Client.Helper;
 using EbookStore.Client.Models;
+using EbookStore.Client.RefitClient;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
@@ -8,10 +9,12 @@ namespace EbookStore.Client.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IWishlistClient _wishlistClient;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IWishlistClient wishlistClient)
     {
         _logger = logger;
+        _wishlistClient = wishlistClient;
     }
 
     public IActionResult Index()
@@ -72,16 +75,18 @@ public class HomeController : Controller
         return RedirectToAction("Shop", "Home");
     }
 
-    public IActionResult SearchGenre(int genreId)
+    public IActionResult SearchGenre(int genreId, string titleSearch)
     {
+        TempData["TitleSearch"] = titleSearch;
         var session = Request.HttpContext.Session;
         session.SetString("Genre", genreId.ToString());
         session.SetString("PageNumber_HomeShop", "1");
         return RedirectToAction("Shop", "Home");
     }
 
-    public IActionResult DateFilter(DateTime startDate, DateTime endDate)
+    public IActionResult DateFilter(DateTime startDate, DateTime endDate, string titleSearch)
     {
+        TempData["TitleSearch"] = titleSearch;
         var session = Request.HttpContext.Session;
         startDate = (startDate != null) ? startDate : DateTime.MinValue;
         endDate = (endDate != null && endDate != DateTime.MinValue) ? endDate  : DateTime.MaxValue;
@@ -96,8 +101,57 @@ public class HomeController : Controller
     }
 
     [HttpPost]
-    public IActionResult ChangePage(string pageNumber)
+    public async Task<IActionResult> AddToWishlistFromShop(int bookId, string titleSearch)
     {
+        TempData["TitleSearch"] = titleSearch;
+        try
+        {
+            UserManager userManager = new UserManager(User);
+            if (userManager.IsLogin())
+            {
+                await _wishlistClient.AddBookToWishlistAsync(bookId, userManager.GetToken());
+                return RedirectToAction("Shop", controllerName: "Home");
+            }
+            return RedirectToAction("Login", "Auth");
+        }
+        catch (Exception ex)
+        {
+            return RedirectToAction("Shop", controllerName: "Home");
+        }
+    }
+
+    public IActionResult Detail(int id)
+    {
+        var session = Request.HttpContext.Session;
+        session.SetString("chosen_id", id.ToString());
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddItemToCartFromShop(int bookId, string titleSearch)
+    {
+        TempData["TitleSearch"] = titleSearch;
+        try
+        {
+            var userManager = new UserManager(this.User);
+            if (userManager.IsLogin())
+            {
+                await _wishlistClient.AddItemtoCartAsync(bookId, userManager.GetToken());
+                await _wishlistClient.RemoveItemsAsync(bookId, userManager.GetToken());
+                return RedirectToAction("Shop", "Home");
+            }
+            return RedirectToAction("Login", "Auth");
+        }
+        catch (Exception ex)
+        {
+            return RedirectToAction("Shop", "Home");
+        }
+    }
+
+    [HttpPost]
+    public IActionResult ChangePage(string pageNumber, string titleSearch)
+    {
+        TempData["TitleSearch"] = titleSearch;
         var session = Request.HttpContext.Session;
         session.SetString("PageNumber_HomeShop", pageNumber);
         return RedirectToAction("Shop", "Home");
